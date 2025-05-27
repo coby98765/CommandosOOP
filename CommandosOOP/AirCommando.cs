41ff696c97a723607b75f0c6ad00fb65ef8bf45f
coby98765 <coby98765@gmail.com>
2025-05-27 12:44:18 +0300

    added SeaCommando

---
+++ CommandosOOP/AirCommando.cs
@@ -0,0 +1,25 @@
+﻿using CommandosOOP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandosOOP.Models
+{
+    internal class AirCommando : Commando
+    {
+        public AirCommando(string name, string nickname) : base(name, nickname) { }
+
+        public void Parachute()
+        {
+            Console.WriteLine($"AirCommando: {Name}, is Parachuting");
+        }
+
+        public override void Attack()
+        {
+            Status = "Attack";
+            Console.WriteLine($"AirCommando: {Name},is Attacking.");
+        }
+    }
+}
